a89bcd88dfe08ad2a1ee7bb446d249d32b221c86
Jarl Sveinung Flø Rasmussen <jarl@jarl.ninja>
2017-03-21 19:46:45 +0100

    Working towards variant

--- server.rb
+++ server.rb
@@ -43,11 +43,12 @@ error do
   puts @e
   "500 server error".to_json
 end
-def list
-  [{:sku => "test", :name => "A product", :price => 10, :available=>false},
-  {:sku => "test2", :name => "Also a product",  :price => 10, :available=>true},
-  {:sku => "test3", :name => "Another product",  :price => 10, :available=>true},
-  {:sku => "test4", :name => "Some product",  :price => 10, :available=>true}]
+def allproducts
+  [
+      {:sku => "ta-qwerty-no", :name => "Tastatur A",:description=>"Et fint tastatur. Norsk qwerty er det vanlige norske tastaturoppsettet", :price => 10, :available=>true, :variations=>[:layout], :layout=>"Norsk qwerty"},
+      {:sku => "ta-qwerty-smi", :variant_of=>"ta-qwerty-no", :name => "Tastatur A",:description=>"Et fint tastatur. Samisk qwerty er det vanlige tastaturoppsett tilpasset for samisk.", :price => 10, :available=>true, :layout=>"Samisk qwerty"},
+      {:sku => "ta-dvorak-no", :variant_of=>"ta-qwerty-no", :name => "Tastatur A",:description=>"Et fint tastatur. Norsk dvorak er oppsett for de litt mer spesielt interesserte..", :price => 10, :available=>true, :layout=>"Norsk dvorak"}
+  ]
 end
 def resource_url(item)
   request.base_url+"/products/"+item[:sku]
@@ -61,15 +62,15 @@ def want_json?
 end
 get '/products' do
   if want_json?
-    list.map{|item|resource_url(item)}.to_json
+    allproducts.map{|item|resource_url(item)}.to_json
   else
     haml :products
   end
 
 end
 get '/products/products-component' do
-  available=  list.select {|product| product[:available]}
-  uris=available.map{|item|resource_url(item)}
+  available=  allproducts.select {|product| product[:available]}
+  uris=available.select{|product|not product.has_key?(:variant_of)}.map{|item|resource_url(item)}
   haml :products_component, :locals => {:items =>uris}
 end
 get '/products/product-component' do
@@ -78,12 +79,27 @@ end
 get '/products/product-details-component' do
   haml :product_detail_component
 end
+def map(item)#TODO make it nice yes?
+  hash={:url => resource_url(item), :sku => item[:sku], :name => item[:name], :description=>item[:description],:price => item[:price], :available=>item[:available],:details => details_url(item) }
+  item[:variations].each do |variation|
+    hash[variation]=item[variation]
+  end if item.has_key? :variations
+  list=[hash]
+  allproducts.select {|product| (product.has_key? :variant_of)&&product[:variant_of]==item[:sku]}.each do|product|
+    hash={:url => resource_url(product), :sku => product[:sku], :name => product[:name],:description=>product[:description], :price => product[:price], :available=>product[:available]}
+    item[:variations].each do |variation|
+      hash[variation]=product[variation]
+    end
+    list<<hash
+  end
+  {:url => resource_url(item), :sku => item[:sku], :name => item[:name], :description=>item[:description],:price => item[:price], :available=>item[:available],:details => details_url(item),:variations=>item[:variations], :variants=>list}
+end
 get '/products/:sku' do
-  item=list.find{|item|item[:sku]==params['sku']}
+  item=allproducts.find{|item|item[:sku]==params['sku']}
   if(item.nil?)
     status 404
   elsif want_json?
-    {:url => resource_url(item), :sku => item[:sku], :name => item[:name], :price => item[:price], :available=>item[:available],:details => details_url(item)}.to_json
+    map(item).to_json
   else
     haml :product, :locals => {:url =>resource_url(item)}
   end
@@ -91,7 +107,7 @@ end
 
 
 get '/products/:sku/details' do
-  item=list.find{|item|item[:sku]==params['sku']}
+  item=allproducts.find{|item|item[:sku]==params['sku']}
   if(item.nil?)
     status 404
   elsif want_json?
